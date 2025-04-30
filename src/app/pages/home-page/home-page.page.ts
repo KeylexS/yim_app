f5a68d0da1a51a78c3cc3fa71b5faa1a3df95c5d
luisd1223 <luisd1223@users.noreply.github.com>
2025-04-29 23:15:29 -0600

    se arregla bug del search icon

--- src/app/pages/home-page/home-page.page.ts
+++ src/app/pages/home-page/home-page.page.ts
@@ -1,24 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButtons, IonIcon, IonButton } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonButtons,
+  IonIcon,
+  IonButton,
+} from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-
+import { addIcons } from 'ionicons';
+import { search } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.page.html',
   styleUrls: ['./home-page.page.scss'],
   standalone: true,
-  imports: [IonButton, IonIcon, IonButtons, IonSearchbar, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [
+    IonButton,
+    IonIcon,
+    IonButtons,
+    IonSearchbar,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    RouterLink,
+  ],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  constructor() {
+    addIcons({ search });
+  }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   searchFunction() {
     console.log('Search button clicked!');
