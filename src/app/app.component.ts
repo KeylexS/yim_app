bc28f2105fa4d23d3fa6c377d7fd2c62b47f2153
cyaos <ehw2003@gmail.com>
2025-05-04 21:36:55 -0600

    feat: changes to the navigation menu for lessons page.
    Changes made so whenever the user presses the classes icon, it changes color and switches to the classes list page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { IonRouterOutlet } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,13 +14,18 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, IonRouterOutlet, RouterLink],
 })
-
 export class AppComponent {
-  constructor(private router: Router) {
+  currentRoute: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url.split('?')[0]; // Handles query params too
+      }
+    });
   }
 
   get showMenu(): boolean {
-    return this.router.url !== '/welcome-page';
+    return this.currentRoute !== '/welcome-page';
   }
 }
