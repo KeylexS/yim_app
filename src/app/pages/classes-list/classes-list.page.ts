2d6d202a5c91f30c69884dd3a16f65a453ef43d9
luisd1223 <luisd1223@users.noreply.github.com>
2025-05-06 21:07:14 -0600

    Modificaciones de flujo, componentes y visuales.
    Se elimina la pagina home-page del flujo. Ahora, la app va directamente a la página de clases. Además, se agrega la barra de búsqueda, se cambia el color del fondo de la aplicación y se alinean los elementos de la barra de búsqueda con el icono de buscar
    
    Co-Authored-By: Elizabeth Huang <129456370+cyaos@users.noreply.github.com>

--- src/app/pages/classes-list/classes-list.page.ts
+++ src/app/pages/classes-list/classes-list.page.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import {
   IonContent,
   IonHeader,
+  IonButtons,
   IonTitle,
   IonToolbar,
   IonCard,
@@ -15,23 +16,23 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonIcon
-} from '@ionic/angular/standalone';
-
-
+  IonIcon, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+  import { addIcons } from 'ionicons';
+  import { search } from 'ionicons/icons';
 
 @Component({
   selector: 'app-classes-list',
   templateUrl: './classes-list.page.html',
   styleUrls: ['./classes-list.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonButton, IonSearchbar, 
     CommonModule,
     FormsModule,
     NgIf,
     NgFor,
     IonContent,
     IonHeader,
+    IonButtons,
     IonTitle,
     IonToolbar,
     IonCard,
@@ -42,18 +43,24 @@ import {
     IonGrid,
     IonRow,
     IonCol,
-    IonIcon
-  ]
-  
+    IonIcon,
+  ],
 })
 export class ClassesListPage implements OnInit {
   classes: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    addIcons({ search });
+  }
 
   ngOnInit() {
-    this.http.get<any>('assets/data/Clases_info.json').subscribe(data => {
+    this.http.get<any>('assets/data/Clases_info.json').subscribe((data) => {
       this.classes = data.classes;
     });
+  }
+
+  searchFunction() {
+    console.log('Search button clicked!');
+    // Add the logic for your search functionality here
   }
 }
